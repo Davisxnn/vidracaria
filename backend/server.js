32d503144722c5f3432dd4a7e98fcce5ee270c61
Davisxnn <davison.wrk@gmail.com>
2026-01-20 14:54:44 -0300

    Ajustes server.js para Railway

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const db = require('./database');
 const express = require('express');
 const path = require('path');
 
@@ -35,9 +36,12 @@ app.get('/', (req, res) => {
 });
 
 // ===== SERVIDOR =====
-app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 
 app.get('/debug/produtos', (req, res) => {
   db.all('PRAGMA table_info(produtos)', (err, rows) => {
